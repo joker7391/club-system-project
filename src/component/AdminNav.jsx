5f7a697b7fcf52c69224ed237995a4412b4eb023
joker7391 <danielaco3091@gmail.com>
2024-09-06 17:26:20 +0800

    Update

--- src/component/AdminNav.jsx
+++ src/component/AdminNav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase.config";
 import { signOut } from "firebase/auth";
+import clubData from "../assets/clubLOGO.png";
 
 const AdminNav = () => {
   const navigate = useNavigate();
@@ -16,7 +17,10 @@ const AdminNav = () => {
   };
   return (
     <nav className="bg-blue-700 p-4 flex justify-between items-center">
-      <h1 className="text-white text-2xl font-bold">Admin</h1>
+      <div className="flex items-center">
+        <h1 className="text-white text-2xl font-bold">Admin</h1>
+        <img className="w-[3em] h-[3em] " src={clubData} alt="logo" />
+      </div>
       <button
         onClick={handleLogout}
         className="bg-blue-500 text-white px-4 py-2 rounded"
